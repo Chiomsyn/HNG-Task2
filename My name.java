d52fc7e2e11471bfb44c37593be7c0a4309c4857
Chioma Ezeamama <58475309+Chiomsyn@users.noreply.github.com>
2021-08-19 05:14:49 +0100

    Create My name.java

---
+++ My name.java
@@ -0,0 +1,12 @@
+package hng_my_name;
+
+public class HNG_My_Name {
+
+    public static void main(String[] args) {
+        
+        String name = "Ezeamama Chioma M.";
+        System.out.println("My name is: " + name);
+        
+    }
+    
+}
